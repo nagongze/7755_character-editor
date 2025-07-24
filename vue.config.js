1a3ab3566e17183c24a4ecf71b5ec4689472f404
ATone <j033312003@gmail.com>
2025-07-24 17:13:12 +0800

    部署腳本

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,10 @@ const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
   transpileDependencies: true,
+  // GitHub Pages 部署設定
+  publicPath: process.env.NODE_ENV === 'production' 
+    ? '/character-editor/' 
+    : '/',
   devServer: {
     https: true,
     port: 8080,
