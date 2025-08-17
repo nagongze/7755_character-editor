e5a67bfd6cda7409368dc093c79c82c86aaccb37
nagongze <68653451+nagongze@users.noreply.github.com>
2025-08-17 20:15:18 +0800

    Update PersonalInfo.vue
    英文版字數上限

--- src/components/Character/PersonalInfo.vue
+++ src/components/Character/PersonalInfo.vue
@@ -5,19 +5,19 @@
       <el-row :gutter="20">
         <el-col :xs=24 :sm=24 :lg="8">
           <el-form-item label="姓名">
-            <TextareaWithCopy v-model="character.personalInfo.name" placeholder="角色的姓名" :rows="1" :max-length="15"
+            <TextareaWithCopy v-model="character.personalInfo.name" placeholder="角色的姓名" :rows="1" :max-length="20"
               @input="updateCharacter" />
           </el-form-item>
         </el-col>
         <el-col :xs=24 :sm=10 :lg="8">
           <el-form-item label="年齡">
-            <TextareaWithCopy v-model="character.personalInfo.age" placeholder="角色的年齡" :rows="1" :max-length="15"
+            <TextareaWithCopy v-model="character.personalInfo.age" placeholder="角色的年齡" :rows="1" :max-length="20"
               @input="updateCharacter" />
           </el-form-item>
         </el-col>
         <el-col :xs=24 :sm=14 :lg="8">
           <el-form-item label="職業">
-            <TextareaWithCopy v-model="character.personalInfo.profession" placeholder="角色的職業" :rows="1" :max-length="15"
+            <TextareaWithCopy v-model="character.personalInfo.profession" placeholder="角色的職業" :rows="1" :max-length="20"
               @input="updateCharacter" />
           </el-form-item>
         </el-col>
@@ -26,13 +26,13 @@
         <el-col :span="24">
           <el-form-item label="來自角色的一句話">
             <TextareaWithCopy v-model="character.personalInfo.quote" placeholder="角色的經典台詞或口頭禪" :rows="2"
-              :max-length="80" @input="updateCharacter" />
+              :max-length="100" @input="updateCharacter" />
           </el-form-item>
         </el-col>
       </el-row>
 
       <el-form-item label="敘述">
-        <TextareaWithCopy v-model="character.personalInfo.description" placeholder="角色的詳細描述" :rows="5" :max-length="700"
+        <TextareaWithCopy v-model="character.personalInfo.description" placeholder="角色的詳細描述" :rows="5" :max-length="1000"
           @input="updateCharacter" />
       </el-form-item>
 
