dd54f52a5a06ba9fbc97180a3f78a0123dd38f09
nagongze <68653451+nagongze@users.noreply.github.com>
2025-08-17 20:12:12 +0800

    Update BasicSettings.vue
    英文版字數上限

--- src/components/Character/BasicSettings.vue
+++ src/components/Character/BasicSettings.vue
@@ -12,17 +12,17 @@
 
       <el-form-item label="基本資訊">
         <TextareaWithCopy v-model="character.basicSettings.basicInfo" placeholder="角色的基本資訊，背景、家庭、MBTI、外表等" :rows="6"
-          :max-length="700" @input="updateCharacter" />
+          :max-length="1000" @input="updateCharacter" />
       </el-form-item>
 
       <el-form-item label="性格">
         <TextareaWithCopy v-model="character.basicSettings.personality" placeholder="角色的性格特徵" :rows="6"
-          :max-length="700" @input="updateCharacter" />
+          :max-length="1000" @input="updateCharacter" />
       </el-form-item>
 
       <el-form-item label="說話風格與習慣">
         <TextareaWithCopy v-model="character.basicSettings.speakingStyle" placeholder="角色的說話方式和風格，可輸入例句" :rows="6"
-          :max-length="700" @input="updateCharacter" />
+          :max-length="1000" @input="updateCharacter" />
       </el-form-item>
     </el-form>
   </div>
